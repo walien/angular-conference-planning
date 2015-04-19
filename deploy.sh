2164c71f1aec61869f7729f4b93e6fa0cce7294d
Elian ORIOU <elian.oriou@4sh.fr>
2015-04-19 13:05:30 +0400

    improve auto deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,11 @@ git checkout -b gh-pages
 git checkout gh-pages
 mkdir build
 git clone -b master git@github.com:walien/angular-conference-planning.git build/
-cd build/app
+cp -R build/app/* .
 git add .
 git commit -m 'Auto deploy'
 git push -u origin gh-pages
+rm -rf build
 git checkout master
+
 
